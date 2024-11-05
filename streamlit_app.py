d938ede0f0bd0a119d1ea3c9e718c31dd0200b7a
SAYALI-008 <sayalium111@gmail.com>
2024-11-05 08:19:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ with st.expander('Data preparation'):
   st.write('**Encoded y**')
   y
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Model training and inference
 ## Train the ML model
